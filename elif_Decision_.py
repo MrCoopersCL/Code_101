4e0ac577552f6cc564d87efa8f9e10b497b1f910
MrCoopersCL <116738892+MrCoopersCL@users.noreply.github.com>
2022-10-27 09:23:13 -0700

    Update elif_Decision_.py
    Revised  comments

--- elif_Decision_.py
+++ elif_Decision_.py
@@ -1,18 +1,27 @@
 #Now we will give our script more options using elif
-cats = 100
-dogs = 50
+pizza = 100
+hotdogs = 50
 turtles = 2
 
-#if dogs > cats:
-    #print('the earth is droooled on')
-#elif dogs == dogs:
-    #print('the earth is doomed')
-#elif turtles == 2:
-    #print('the earth is slow')
-
 #elif allows the program to have more branches
+if pizza == turtles:
+    print('we have one slice for every turtle')
+elif pizza > turtles:
+    print('the turtles will feast!')
+elif pizza < turtles:
+    print('looks like the turtles will have to share.')
+else:
+    print("Hmmm...something isn't quite right")
 
-#accepting inputs into the
+#The first True condition of an if statement is the one that executes. 
+if pizza > hotdogs:
+    print('looks like its pizza for dinner')
+elif hotdogs == hotdogs:
+    print('the two variables are equivalent')
+elif turtles == 2:
+    print('there are two turtles')
+
+#It is also useful to assign a variable an input value and then using that value as a test condition. 
 dogs = input('Please enter number of dogs :')
 cats = input('Please enter the number of cats :')
 
@@ -22,3 +31,7 @@ elif dogs < cats:
     print('Hide the tuna fish... the cats are here.')
 else:
     print('I dont know what you mean.')
+    
+    
+#Go ahead and read through this code as many times as needed so that it makes sense. 
+#Next, declare different value ther variables above. What happens?
