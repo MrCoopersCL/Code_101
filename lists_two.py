45ac5021575b3fe699475eac1744cf5b9c9e5a7f
MrCoopersCL <116738892+MrCoopersCL@users.noreply.github.com>
2022-11-02 15:19:09 -0700

    Update lists_two.py
    Added more examples and clarified some comments.

--- lists_two.py
+++ lists_two.py
@@ -43,3 +43,24 @@ print(list_list[1][0], list_list[1][1], list_list[1][2])
 print(list_list[2])
 print(list_list[2][0], list_list[2][1], list_list[2][2])
 
+#This syntax is very important to remember!
+#This is because if the wrong index is used then append, insert, and pop can lead to incorrect data. 
+
+#A question might go something like this:
+#Remove the first item from the first list, the second item from the second list, and the third item from the third list. 
+list_list[0].pop(0)
+list_list[1].pop(1)
+list_list[2].pop(2)
+
+print(list_list)
+dotbar(50)
+#here is another on add the string 'name' to the end of each nested list. 
+list_list[0].append('name')
+list_list[1].append('name')
+list_list[2].append('name')
+
+print(list_list)
+
+#As you can see there are many things that can be done with lists. 
+#The important thing is to know how the index references an item in the list. 
+#This is how you can make sure that the method being run on the list is the correct one. 
