e7ba6458c3db5875f976eed8da8b062f7977813e
MrCoopersCL <116738892+MrCoopersCL@users.noreply.github.com>
2022-11-03 13:13:05 -0700

    Add files via upload
    Uploaded a deciphering assignment.
    Try to solve it in as few lines of code possible!

---
+++ decipher_assignment.py
@@ -0,0 +1,51 @@
+#Below this comment is a list of characters in binary.
+binary_list = [0b1101000,
+0b1110100,
+0b1110100,
+0b1110000,
+0b1110011,
+0b111010,
+0b101111,
+0b101111,
+0b1110011,
+0b1110111,
+0b1100001,
+0b1111001,
+0b101110,
+0b1101111,
+0b1100110,
+0b1100110,
+0b1101001,
+0b1100011,
+0b1100101,
+0b101110,
+0b1100011,
+0b1101111,
+0b1101101,
+0b101111,
+0b1111001,
+0b1110110,
+0b1100101,
+0b1100011,
+0b1000111,
+0b110110,
+0b1110111,
+0b1111010,
+0b1011001,
+0b1110001,
+0b1110010,
+0b1000001,
+0b110010,
+0b1111001,
+0b1110110,
+0b1111000]
+
+#use your knowledge of lists and for loops to decode the characters.
+#Put your code beneath this comment
+
+
+#Copy and paste the characters into a URL bar in a browser.
+
+#Take a screenshot of the web page (the artifact) and paste it to a google doc.
+
+#Share the document with me. 
